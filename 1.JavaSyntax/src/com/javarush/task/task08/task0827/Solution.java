ba53dfacba7194dcc056ecc5165783aa47b830b1
e51 <fvdc@ya.ru>
2020-01-18 15:53:46 +0300

    level 8 done

--- 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task08.task0827;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /* 
 Работа с датой
@@ -12,6 +15,32 @@ public class Solution {
     }
 
     public static boolean isDateOdd(String date) {
+
+        // hate dates !!!
+
+        SimpleDateFormat inputDateFormat = new SimpleDateFormat("MMM d yyyy", Locale.ENGLISH);
+
+        try {
+            Date givenDate = inputDateFormat.parse(date);
+
+            Date startYearDate = new Date(givenDate.getYear(), 0, 1);
+
+//            System.out.println(givenDate.getYear());
+//            System.out.println(givenDate);
+//            System.out.println(startYearDate);
+
+            long value = (givenDate.getTime() - startYearDate.getTime()) / 1000 / 60 / 60 / 24 + 1;
+
+//            System.out.println(value);
+
+            if (value % 2 == 0) {
+                return false;
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         return true;
     }
 }
