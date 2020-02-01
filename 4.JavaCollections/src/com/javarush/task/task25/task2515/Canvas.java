1de0b0f99c753959d0e1636d9723c6dee858887d
e51 <fvdc@ya.ru>
2020-02-01 14:41:29 +0300

    Big game: Space 9/18 done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -37,4 +37,23 @@ public class Canvas {
 
         }
     }
+
+    public void clear() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[j][i]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
