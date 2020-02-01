72edb173cc71ed450bd535dced6e6172523c7f41
e51 <fvdc@ya.ru>
2020-02-01 14:27:42 +0300

    Big game: Space 7/18 done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -11,7 +11,7 @@ public class Bomb extends BaseObject {
     }
 
     @Override
-    public void draw() {
+    public void draw(Canvas canvas) {
 
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -11,7 +11,7 @@ public class Rocket extends BaseObject {
     }
 
     @Override
-    public void draw() {
+    public void draw(Canvas canvas) {
 
     }
 }
