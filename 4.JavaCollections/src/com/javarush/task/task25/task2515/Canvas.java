930124785d56f426aab0a8d013707b75007de899
e51 <fvdc@ya.ru>
2020-02-01 14:37:10 +0300

    Big game: Space 8/18 done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -22,4 +22,19 @@ public class Canvas {
     public char[][] getMatrix() {
         return matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        if (x >= 0 && y >= 0 && y < matrix.length && x < matrix[0].length)
+        matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0)
+                    setPoint(x + j, y + i, c);
+            }
+
+        }
+    }
 }
