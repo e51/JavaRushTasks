68558ba3d3d97fc216eef4678e4b1045d603c368
e51 <fvdc@ya.ru>
2020-02-02 18:34:59 +0300

    Big task: Fabrics 3/4 done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task37.task3702.female;
+
+import com.javarush.task.task37.task3702.Human;
+import com.javarush.task.task37.task3702.male.KidBoy;
+import com.javarush.task.task37.task3702.male.Man;
+import com.javarush.task.task37.task3702.male.TeenBoy;
+
+public class FemaleFactory {
+    public Human getPerson(int age) {
+
+        Human human = null;
+
+        if (age <= KidGirl.MAX_AGE) {
+            human = new KidGirl();
+        } else if (age <= TeenGirl.MAX_AGE) {
+            human = new TeenGirl();
+        } else {
+            human = new Woman();
+        }
+
+        return human;
+    }
+}
