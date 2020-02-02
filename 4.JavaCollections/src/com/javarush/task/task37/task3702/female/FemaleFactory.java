fcc1e0b9d8520cda12aa93c8a34d6cc39a8c8472
e51 <fvdc@ya.ru>
2020-02-02 18:41:09 +0300

    Big task: Fabrics 4/4 done

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/female/FemaleFactory.java
@@ -1,11 +1,12 @@
 package com.javarush.task.task37.task3702.female;
 
+import com.javarush.task.task37.task3702.AbstractFactory;
 import com.javarush.task.task37.task3702.Human;
 import com.javarush.task.task37.task3702.male.KidBoy;
 import com.javarush.task.task37.task3702.male.Man;
 import com.javarush.task.task37.task3702.male.TeenBoy;
 
-public class FemaleFactory {
+public class FemaleFactory implements AbstractFactory {
     public Human getPerson(int age) {
 
         Human human = null;
