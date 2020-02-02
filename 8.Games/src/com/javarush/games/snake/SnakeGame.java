27abfab43302133a46413c77c4bbae1ecbdec923
e51 <fvdc@ya.ru>
2020-02-02 19:32:21 +0300

    Big games: Snake 6/20 done

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -6,6 +6,8 @@ public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
 
+    private Snake snake;
+
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
@@ -13,6 +15,8 @@ public class SnakeGame extends Game {
     }
 
     private void createGame() {
+
+        snake = new Snake(WIDTH/2, HEIGHT/2);
 
         drawScene();
 
@@ -24,6 +28,8 @@ public class SnakeGame extends Game {
                 setCellColor(x, y, Color.GREEN);
             }
         }
+
+        snake.draw(this);
 
     }
 
