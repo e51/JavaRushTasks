a4468aba824cbd29146d69304f327611a67ad687
e51 <fvdc@ya.ru>
2020-02-03 15:26:59 +0300

    Big game: Hippodrome 6/16 done

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    public static Hippodrome game;
     private List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
@@ -14,6 +16,13 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-        
+        game = new Hippodrome(new ArrayList<>());
+        Horse horse1 = new Horse("Bukovka", 3, 0);
+        Horse horse2 = new Horse("Lyutik", 3, 0);
+        Horse horse3 = new Horse("Schastliviy", 3, 0);
+
+        game.getHorses().add(horse1);
+        game.getHorses().add(horse2);
+        game.getHorses().add(horse3);
     }
 }
