65a325c71ed86bdf7624a43dead5f5610c19f678
e51 <fvdc@ya.ru>
2020-02-03 21:57:29 +0300

    Big Game: Snake (level 22) 18/18 final

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task23.task2312;
 
-import java.util.Objects;
-
 public class SnakeSection {
     private int x;
     private int y;
+
 
     public SnakeSection(int x, int y) {
         this.x = x;
@@ -23,13 +22,19 @@ public class SnakeSection {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        SnakeSection section = (SnakeSection) o;
-        return x == section.x &&
-                y == section.y;
+
+        SnakeSection that = (SnakeSection) o;
+
+        if (x != that.x) return false;
+        if (y != that.y) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y);
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 }
