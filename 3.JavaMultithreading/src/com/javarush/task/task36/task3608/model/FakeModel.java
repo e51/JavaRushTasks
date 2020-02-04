32e8224d058881cb1ac1545ca9622f8265552a12
e51 <fvdc@ya.ru>
2020-02-04 18:39:14 +0300

    Big task: MVC 1/9 done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.getUsers().add(new User("A", 1, 1));
+        modelData.getUsers().add(new User("B", 2, 1));
+    }
+}
