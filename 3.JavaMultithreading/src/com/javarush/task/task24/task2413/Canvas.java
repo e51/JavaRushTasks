d8156cd0f61346b1dd91c7b4851df92684b44a66
e51 <fvdc@ya.ru>
2020-02-04 14:22:33 +0300

    Big game: Arkanoid 11/20 done

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -52,4 +52,18 @@ public class Canvas {
             }
         }
     }
+
+    public void clear() {
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public void print() {
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
