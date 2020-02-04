be2408ae930f4cd1a639cb94b6effff3a45f29c3
e51 <fvdc@ya.ru>
2020-02-04 20:10:32 +0300

    Big task: MVC 7/9 done

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -27,6 +27,12 @@ public class Controller {
         usersView.refresh(model.getModelData());
     }
 
+    public void onOpenUserEditForm(long userId) {
+        model.loadUserById(userId);
+        editUserView.refresh(model.getModelData());
+    }
+
+
 
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
