3c0a69acda797e40d600a7d10d48ba59927ad76e
e51 <fvdc@ya.ru>
2020-02-05 18:43:27 +0300

    Big task: Chat 6/22 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,49 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
     public static void main(String[] args) {
+
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("We are ON!");
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    serverSocket.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            }
+            new Handler(socket).start();
+        }
 
     }
 }
