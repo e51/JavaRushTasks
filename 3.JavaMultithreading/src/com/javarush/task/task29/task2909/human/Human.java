6aaebedfd569bf3dda1cd6bac9f0fa85d8df5235
e51 <fvdc@ya.ru>
2020-02-05 13:18:05 +0300

    Big task: Refactoring 9/15 done

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -11,13 +11,14 @@ public class Human implements Alive {
     protected String name;
     private List<Human> children = new ArrayList<>();
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
+
+    public class Size {
+        public int height;
+        public int weight;
+    }
 
     public String getPosition() {
         return "Человек";
@@ -39,11 +40,11 @@ public class Human implements Alive {
         children.remove(human);
     }
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -78,6 +79,6 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 }
