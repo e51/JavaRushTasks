9c9e6e9e8067eccce980f8a6137fb8b3e5960ad2
e51 <fvdc@ya.ru>
2020-02-05 21:03:25 +0300

    Big task: Chat 19/22 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,9 +1,69 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class BotClient extends Client {
     public class BotSocketThread extends SocketThread {
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            if (message != null) {
+                ConsoleHelper.writeMessage(message);
+                SimpleDateFormat format = null;
+                if (message.contains(": ")) {
+                    String[] strings = message.split(": ");
+                    if (strings.length == 2 && strings[1] != null) {
+                        String name = strings[0];
+                        String text = strings[1];
+                        switch (text) {
+                            case "дата":
+                                format = new SimpleDateFormat("d.MM.YYYY");
+                                break;
+                            case "день":
+                                format = new SimpleDateFormat("d");
+                                break;
+                            case "месяц":
+                                format = new SimpleDateFormat("MMMM");
+                                break;
+                            case "год":
+                                format = new SimpleDateFormat("YYYY");
+                                break;
+                            case "время":
+                                format = new SimpleDateFormat("H:mm:ss");
+                                break;
+                            case "час":
+                                format = new SimpleDateFormat("H");
+                                break;
+                            case "минуты":
+                                format = new SimpleDateFormat("m");
+                                break;
+                            case "секунды":
+                                format = new SimpleDateFormat("s");
+                                break;
+
+                        }
+                        if (format != null) {
+                            sendTextMessage(String.format("Информация для %s: %s", name, format.format(Calendar.getInstance().getTime())));
+                        }
+                    }
+                }
+            }
+        }
     }
+
 
     @Override
     protected SocketThread getSocketThread() {
