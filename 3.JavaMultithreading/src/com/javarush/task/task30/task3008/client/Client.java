13610d92dab2e01ef0248a17a1b7cd347b4f24da
e51 <fvdc@ya.ru>
2020-02-05 20:29:44 +0300

    Big task: Chat 18/22 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -13,8 +13,6 @@ public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
 
-
-
     public class SocketThread extends Thread {
 
         @Override
