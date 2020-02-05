25cdeb25a3b0f0159184e9a58dc38f1af1563ff7
e51 <fvdc@ya.ru>
2020-02-05 18:48:13 +0300

    Big task: Chat 7/22 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,11 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     private static class Handler extends Thread {
         private Socket socket;
@@ -16,6 +19,17 @@ public class Server {
         @Override
         public void run() {
             super.run();
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> entry : connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error");
+            }
         }
     }
 
