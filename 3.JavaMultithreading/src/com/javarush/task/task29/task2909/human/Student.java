e8765dbe6fc43185ea6f3ab1b98f3454f829b143
e51 <fvdc@ya.ru>
2020-02-05 12:52:05 +0300

    Big task: Refactoring 7/15 done

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -26,12 +26,8 @@ public class Student extends UniversityPerson {
     public void learn() {
     }
 
-    public void incAverageGradeBy01() {
-        averageGrade += 0.1;
-    }
-
-    public void incAverageGradeBy02() {
-        averageGrade += 0.2;
+    public void incAverageGrade(double delta) {
+        averageGrade += delta;
     }
 
     public void setCourse(int value) {
@@ -42,12 +38,12 @@ public class Student extends UniversityPerson {
         averageGrade = value;
     }
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
