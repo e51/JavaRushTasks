5059a6ae7411819ddf1ec05db6cb2183e73a13cb
e51 <fvdc@ya.ru>
2020-02-05 12:13:22 +0300

    Big task: Refactoring 3/15 done

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human {
+public class Human implements Alive {
     public static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
-    protected int course;
     private List<Human> children = new ArrayList<>();
 
     protected int[] size;
@@ -61,10 +60,6 @@ public class Human {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getCourse() {
-        return course;
     }
 
     public void live() {
--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -9,6 +9,7 @@ public class Student extends Human {
     private String university;
     private Date beginningOfSession;
     private Date endOfSession;
+    private int course;
 
     public Student(String name, int age, double averageGrade) {
         super(name, age);
@@ -17,6 +18,10 @@ public class Student extends Human {
 
     public void live() {
         learn();
+    }
+
+    public int getCourse() {
+        return course;
     }
 
     public void learn() {
